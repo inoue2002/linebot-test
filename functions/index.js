0fc877acb9645a45f7ab0b8a69bdcf53d445b611
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-03-24 14:02:10 +0900

    メニューを変えるAPI

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,33 @@
 const functions = require("firebase-functions");
 const line = require("@line/bot-sdk");
 require("dotenv").config();
+const client = new line.Client({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+});
 
 exports.linebot = functions
-  .region('asia-northeast1')
+  .region("asia-northeast1")
   .https.onRequest(async (req, res) => {
-  const client = new line.Client({
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET,
+    for (const event of req.body.events) {
+      let message;
+      switch (event.type) {
+        case "message":
+          message = await messageFunc(event);
+          break;
+        case "follow":
+          message = { type: "text", text: "友達登録ありがとう！" };
+          break;
+        default:
+          message = {};
+          break;
+      }
+      if (message !== {}) {
+        await client.replyMessage(event.replyToken, message);
+      }
+    }
+    res.json({});
   });
-  for (const event of req.body.events) {
-    let message;
-    switch (event.type) {
-      case "message":
-        message = await messageFunc(event);
-        break;
-      case "follow":
-        message = { type: "text", text: "友達登録ありがとう！" };
-        break;
-      default:
-        message = {};
-        break;
-    }
-    if (message !== {}) {
-      await client.replyMessage(event.replyToken, message);
-    }
-  }
-  res.json({});
-});
 
 async function messageFunc(event) {
   let message;
@@ -55,10 +55,43 @@ async function textFunc(event) {
       message = { type: "text", text: "レッスン履歴です！" };
     case "＞レッスン予約確認":
       message = { type: "text", text: "レッスン予約確認です！" };
-      default :
-      message = {type:'text',text:`${event.message.text}を受け取ったよ！`}
-      break
-
+    default:
+      message = { type: "text", text: `${event.message.text}を受け取ったよ！` };
+      break;
   }
   return message;
 }
+
+//リッチメニューを切り替える関数
+//req.body:{userId:'xxxxxxxxxx'}
+exports.changeMenu = functions.https.onRequest(async (req, res) => {
+  const userId = req.body.userId;
+  //1or2
+  const array = [1, 2];
+  const randomId = array[Math.floor(Math.random() * array.length)];
+  let menuId;
+  switch (randomId) {
+    case 1:
+      menuId = "richmenu-2f36fcd44da48af5bd26276b0f79e43e";
+      break;
+    case 2:
+      menuId = "richmenu-fc9f09ef5de0722388863b1f9147d067";
+      break;
+  }
+
+  //メニューを変える
+  await client.linkRichMenuToUser(userId, menuId);
+
+  console.log(`-----------------------`);
+  console.log(req.body);
+  console.log(`-----------------------`);
+
+  //CORS用にAccess-Control-Allow系ヘッダを追加
+  //許可するサイトを指定
+  res.set("Access-Control-Allow-Origin", "http://google.com");
+  //DELETEだけは拒否
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+  //Content-Typeのみを許可
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.json("ok");
+});
